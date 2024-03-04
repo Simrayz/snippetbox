52f4ee8f5383a55c73a2726fcc0ac253c0978a2a
Simen Abelsen <simenabelsen@hotmail.no>
2024-03-04 23:48:58 +0100

    12.2 Testing http handlers and middleware - END

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -13,6 +13,10 @@ import (
 	"snippetbox.simrayz.net/internal/validator"
 )
 
+func ping(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("OK"))
+}
+
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	snippets, err := app.snippets.Latest()
 	if err != nil {
